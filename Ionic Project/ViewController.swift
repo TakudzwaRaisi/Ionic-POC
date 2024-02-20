901de35ffb5e014b9e8fcc7fa3f37a710c96a142
Takudzwa Raisi <takudzwa.raisi@ayoba.me>
2024-02-20 15:27:49 +0200

    Initial Commit

---
+++ Ionic Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ionic Project
+//
+//  Created by Takudzwa Raisi on 2024/02/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
